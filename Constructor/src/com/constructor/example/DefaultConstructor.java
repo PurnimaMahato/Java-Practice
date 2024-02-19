542cd563e8b13e5563ea7ab26b8d94759a46a42d
PurnimaMahato <purnimamahato782@gmail.com>
2024-02-19 14:01:23 +0530

    constructor

---
+++ Constructor/src/com/constructor/example/DefaultConstructor.java
@@ -0,0 +1,13 @@
+package com.constructor.example;
+
+public class DefaultConstructor {
+	public DefaultConstructor(){
+		
+		System.out.println("Default constructor");
+	}
+
+	public static void main(String args[]) {
+		DefaultConstructor obj = new DefaultConstructor();
+	}
+}
+
---
+++ Constructor/src/com/constructor/example/ParameterizedConstructor.java
@@ -0,0 +1,22 @@
+package com.constructor.example;
+
+public class ParameterizedConstructor {
+	ParameterizedConstructor(double d){
+		System.out.println("1 argument .");
+		System.out.println(d);
+	}
+	ParameterizedConstructor(int a , int b){
+		System.out.println("2 argument .");
+		System.out.println(a);
+		System.out.println(b);
+	}
+	
+
+	public static void main(String[] args) {
+		ParameterizedConstructor obj1 = new ParameterizedConstructor(5.5);
+		ParameterizedConstructor obj2 = new ParameterizedConstructor(5,8);
+		
+
+	}
+
+}
